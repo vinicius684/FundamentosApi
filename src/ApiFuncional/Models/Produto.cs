b731f8249ca0c71d3dd96941f147f12ff08d7ecf
Vinícius Henriques <viniciuspinholi6@gmail.com>
2024-01-16 12:32:27 -0300

    Model produto e Data Annotations

---
+++ src/ApiFuncional/Models/Produto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiFuncional.Models
+{
+    public class Produto
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é Obrigratório")]
+        public string? Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é Obrigratório")]
+        [Range(1, int.MaxValue, ErrorMessage = "O preço deve ser maior que zero")]
+        public decimal Preco { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é Obrigratório")]
+        public int QUantidadeEstoque { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é Obrigratório")]
+        public string? Descricao { get; set; }
+    }
+}
