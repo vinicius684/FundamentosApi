ff124f66675df4462623bcb95289ebd35676b7b6
Vinícius Henriques <viniciuspinholi6@gmail.com>
2024-01-18 09:39:09 -0300

    Implementado JWT

---
+++ src/ApiFuncional/Models/JwtSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiFuncional.Models
+{
+    public class JwtSettings
+    {
+        public string? Segredo { get; set; }//chave
+        public int ExpiracaoHoras { get; set; }
+        public string? Emissor { get; set; }//Aplicação que esta emitindo o token
+        public string? Audiencia { get;}//onde esse token é válido
+    }
+}
--- src/ApiFuncional/Program.cs
+++ src/ApiFuncional/Program.cs
@@ -1,6 +1,10 @@
 using ApiFuncional.Data;
+using ApiFuncional.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,7 +27,34 @@ builder.Services.AddDbContext<ApiDbContext>(options => //Suporte ao db Context
 builder.Services.AddIdentity<IdentityUser, IdentityRole>()//Informando uso do Identity - Autenticaçãoe Autorização
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApiDbContext>();
-    
+
+// Pegando o Token e gerando a chave encodada
+var JwtSettingsSection = builder.Configuration.GetSection("JwtSettings");//pegando dados especificados na app settings/configuration section
+builder.Services.Configure<JwtSettings>(JwtSettingsSection);//Configurando JWT com os dados da minha section
+
+var jwtSettings = JwtSettingsSection.Get<JwtSettings>(); //instancia da minha classe populada/buscando
+var key = Encoding.ASCII.GetBytes(jwtSettings.Segredo);//criada chave
+
+builder.Services.AddAuthentication(options =>//Adicionando e Configurando autenticação
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;//esquema padrão de autenticação vai ser JWT
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;//desafio par verificar se o token vai ser válido
+}).AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = true;//preciso trabalhar dentro de https
+    options.SaveToken = true;//permitir que o token seja salvo apos uma uma autenticação com sucesso
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = jwtSettings.Audiencia,
+        ValidIssuer = jwtSettings.Emissor
+    };
+});
+
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -35,7 +66,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthentication();//
+app.UseAuthentication();//Informando uso
 
 app.UseAuthorization();
 
