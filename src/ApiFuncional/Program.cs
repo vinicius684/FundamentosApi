37e44253b43178481d066c0b0f1e7c5e261dd25a
Vinícius Henriques <viniciuspinholi6@gmail.com>
2024-01-18 16:03:29 -0300

    Extensions Methods para separar a classe program em responsabilidades

--- src/ApiFuncional/Program.cs
+++ src/ApiFuncional/Program.cs
@@ -1,3 +1,4 @@
+using ApiFuncional.Configuration;
 using ApiFuncional.Data;
 using ApiFuncional.Models;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -9,92 +10,12 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers()  
-    .ConfigureApiBehaviorOptions(options =>//suprimindo validação de entidade do asp.net
-    { 
-        options.SuppressModelStateInvalidFilter = true;//Se a model estiver num formato inválido, suprima(ignore)
-    
-    });
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("Development", builder =>
-                builder
-                    .AllowAnyOrigin()
-                    .AllowAnyMethod()
-                    .AllowAnyHeader());
-
-    options.AddPolicy("Production", builder =>
-                builder
-                    .WithOrigins("https://localhost:9000")
-                    .WithMethods("POST")
-                    .AllowAnyHeader());
-});
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>//config swagger
-{//suporte a jwt no swagger
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme//tipo de token, esquema de segurança do OpenApi
-    {
-        Description = "Insira o token JWT desta maneira: Bearer {seu token}",
-        Name = "Authorization",
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.ApiKey
-    });
-
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                }
-            },
-            new string[] {}
-        }
-    });
-});
-
-
-builder.Services.AddDbContext<ApiDbContext>(options => //Suporte ao db Context
-{ 
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")); 
-});
-
-builder.Services.AddIdentity<IdentityUser, IdentityRole>()//Informando uso do Identity - Autenticaçãoe Autorização
-    .AddRoles<IdentityRole>()
-    .AddEntityFrameworkStores<ApiDbContext>();
-
-// Pegando o Token e gerando a chave encodada
-var JwtSettingsSection = builder.Configuration.GetSection("JwtSettings");//pegando dados especificados na app settings/configuration section
-builder.Services.Configure<JwtSettings>(JwtSettingsSection);//Configurando JWT com os dados da minha section
-
-var jwtSettings = JwtSettingsSection.Get<JwtSettings>(); //instancia da minha classe populada/buscando
-var key = Encoding.ASCII.GetBytes(jwtSettings.Segredo);//criada chave //encoding de uma sequencia de bytes da minha chave
-
-builder.Services.AddAuthentication(options =>//Adicionando e Configurando autenticação
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;//esquema padrão de autenticação vai ser JWT
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;//desafio par verificar se o token vai ser válido
-}).AddJwtBearer(options =>
-{
-    options.RequireHttpsMetadata = true;//preciso trabalhar dentro de https
-    options.SaveToken = true;//permitir que o token seja salvo apos uma uma autenticação com sucesso
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        IssuerSigningKey = new SymmetricSecurityKey(key),
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidAudience = jwtSettings.Audiencia,
-        ValidIssuer = jwtSettings.Emissor
-    };
-});
-
+builder
+    .AddApiConfig()
+    .AddCorsConfig()
+    .AddSwaggerConfig()
+    .AddDbContextConfig()
+    .AddIdentityConfig();
 
 
 var app = builder.Build();
